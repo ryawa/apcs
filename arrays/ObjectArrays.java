33f3983cb25bc211a6da0af42c01578f1d769aac
ryawa <22625346+ryawa@users.noreply.github.com>
2024-09-23 11:27:18 -0400

    Create ObjectArrays.java

---
+++ arrays/ObjectArrays.java
@@ -0,0 +1,40 @@
+public class ObjectArrays {
+    public static void main(String[] args) {
+        Car[] cars = new Car[3];
+        cars[0] = new Car(2020, "Ford", "F-150");
+        cars[1] = new Car(2017, "Toyota", "Prius");
+        cars[2] = new Car(2019, "Honda", "Civic");
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+        System.out.println();
+
+        modifyArray(cars);
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+        System.out.println();
+
+        reallocateArray(cars);
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+        System.out.println();
+    }
+
+    private static void modifyArray(Car[] arr) {
+        arr[0] = new Car(2023, "Tesla", "Cybertruck");
+        for (int i = 1; i < arr.length; i++) {
+            if (cars[i].getMake().equals("Toyota")) {
+                cars[i].setModel("Highlander");
+            }
+        }
+    }
+
+    private static void reallocateArray(Car[] arr) {
+        arr = new Car[3];
+        arr[0] = new Car(2020, "Ford", "F-150");
+        arr[1] = new Car(2017, "Toyota", "Prius");
+        arr[2] = new Car(2019, "Honda", "Civic");
+    }
+}
