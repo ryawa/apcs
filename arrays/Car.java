d93d9f0608131dbfa4da4f9c111cc1fb54c9f52a
ryawa <22625346+ryawa@users.noreply.github.com>
2024-09-23 11:16:31 -0400

    Create Car.java

---
+++ arrays/Car.java
@@ -0,0 +1,35 @@
+public class Car {
+    private int year;
+    private String make;
+    private String model;
+
+    public Car(int year, String make, String model) {
+        this.year = year;
+        this.make = make;
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
