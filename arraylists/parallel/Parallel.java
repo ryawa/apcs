f0b97f295f8a2ed895e2fe03d8b1ce963c44d2b8
ryawa <22625346+ryawa@users.noreply.github.com>
2024-10-07 10:25:37 -0400

    Update Parallel.java

--- arraylists/parallel/Parallel.java
+++ arraylists/parallel/Parallel.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Parallel {
     public static void main(String[] args) {
         Score[] scores = {
@@ -5,6 +7,10 @@ public class Parallel {
             new Score("David", 87),
             new Score("Arthur", 95)
         };
+        for (int i = 0; i < scores.length; i++) {
+            System.out.println(scores[i].getName() + " " + scores[i].getScore());
+        }
+        Arrays.sort(scores);
         for (int i = 0; i < scores.length; i++) {
             System.out.println(scores[i].getName() + " " + scores[i].getScore());
         }
