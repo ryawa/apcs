4b313b33e664ccc1998bb5eea9b4e18ef0602d71
ryawa <22625346+ryawa@users.noreply.github.com>
2024-10-07 09:36:52 -0400

    Create Score.java

---
+++ arraylists/parallel/Score.java
@@ -0,0 +1,17 @@
+public class Score {
+    private String name;
+    private int score;
+
+    public Score(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
