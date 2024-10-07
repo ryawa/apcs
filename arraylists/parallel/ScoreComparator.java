8a96ee7c1610b91903517e5f61cc45a242eb666c
ryawa <22625346+ryawa@users.noreply.github.com>
2024-10-07 10:24:25 -0400

    Delete arraylists/parallel/ScoreComparator.java

--- arraylists/parallel/ScoreComparator.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ScoreComparator implements Comparator<Score> {
-    public int compareTo(Score o1, Score o2) {
-        return o1.getScore() - o2.getScore();
-    }
-}
