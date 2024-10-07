5dd2e6d83ee03edd788ecacecb01147e4b84df3a
ryawa <22625346+ryawa@users.noreply.github.com>
2024-10-07 09:39:41 -0400

    Update Parallel.java

--- arraylists/parallel/Parallel.java
+++ arraylists/parallel/Parallel.java
@@ -4,7 +4,7 @@ public class Parallel {
             new Score("John", 99),
             new Score("David", 87),
             new Score("Arthur", 95)
-        }
+        };
         for (int i = 0; i < scores.length; i++) {
             System.out.println(scores[i].getName() + " " + scores[i].getScore());
         }
